45445bb016acffea01b3d67ab08d26406926461a
Victoria Ayala <139914452+vimayala@users.noreply.github.com>
2025-02-01 22:22:33 -0800

    Fixing CSS

--- src/blockyanimal.js
+++ src/blockyanimal.js
@@ -1,4 +1,4 @@
-// blockyanimal.js
+// BlockyAnimal.js
 // Sourced from ColoredPoint.js (c) 2012 matsuda with CSE 160 Additional functionality
 // Vertex shader program
 
@@ -78,12 +78,13 @@ var g_seconds = performance.now() / 1000.0 - g_startTime;
 var g_yellowAnimation = true;
 var g_magentaAnimation = false;
 var g_walkingAnimation = true;
+var g_legAnimation = true;
+
 
 let g_lastX = null;
 let g_lastY = null;
 let g_sensitivity = 0.5; // Adjust for smoother/faster rotation
 let g_isDragging = false; 
-
 
 // Asked ChatGPT for this
 function handleMouseMove(event) {
@@ -123,7 +124,6 @@ function clearCanvas(){
 }
 
 function setUpWebGL(){
-    // Retrieve <canvas> element
     canvas = document.getElementById('webgl');
 
     gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
@@ -208,26 +208,20 @@ function addActionForHTMLUI(){
         renderScene(); 
     };
 
-    // // Shape Button Events
-    // document.getElementById('pointButton').onclick = function () {g_selectedType = POINT};
-    // document.getElementById('triButton').onclick = function () {g_selectedType = TRIANGLE};
-    // document.getElementById('circleButton').onclick = function () {g_selectedType = CIRCLE};
+    document.getElementById('animateLegButtonON').onclick = function () {g_legAnimation = true};
+    document.getElementById('animateLegButtonOFF').onclick = function () {g_legAnimation = false};
 
-    document.getElementById('animateYellowButtonON').onclick = function () {g_yellowAnimation = true};
-    document.getElementById('animateYellowButtonOFF').onclick = function () {g_yellowAnimation = false};
-
-    document.getElementById('animateMagentaButtonON').onclick = function () {g_magentaAnimation = true};
-    document.getElementById('animateMagentaButtonOFF').onclick = function () {g_magentaAnimation = false};
+    document.getElementById('animateFeetButtonON').onclick = function () {g_magentaAnimation = true};
+    document.getElementById('animateFeetButtonOFF').onclick = function () {g_magentaAnimation = false};
 
     document.getElementById('animateDogWalkingButtonON').onclick = function () {g_walkingAnimation = true};
     document.getElementById('animateDogWalkingButtonOFF').onclick = function () {g_walkingAnimation = false};
 
 
-    document.getElementById('magentaSlider').addEventListener('mousemove', function() { 
+    document.getElementById('feetSlider').addEventListener('mousemove', function() { 
         g_MagentaAngle = -this.value; 
         renderScene(); 
     });
-
 
     document.getElementById('dogLegSlider').addEventListener('mousemove', function() { 
         g_LegAngle = -this.value; 
@@ -248,7 +242,6 @@ function addActionForHTMLUI(){
         g_globalAngleZ = this.value; 
         renderScene(); 
     });
-
 }
  
 
@@ -269,18 +262,18 @@ function updateAnimationAngles(){
 
     }
     if(g_magentaAnimation){
-        g_MagentaAngle = 45 * Math.sin(2.5*g_seconds);
-
+        g_MagentaAngle = 45 * Math.sin(2.5 * g_seconds);
     }
 
     if(g_walkingAnimation){
         g_walkingAngle = 7 * Math.sin((3 * g_seconds));
-        g_BodyAngle = 45 * Math.sin(2.5*g_seconds);
-        g_LegAngle = 7 * Math.sin((3 * g_seconds));
+        g_BodyAngle = 45 * Math.sin(2.5 * g_seconds);
+        // g_LegAngle = 7 * Math.sin((3 * g_seconds));
     }
 
-    // if(g_legAnimation){
-    // }
+    if(g_legAnimation){
+        g_LegAngle = 7 * Math.sin((3 * g_seconds));
+    }
 }
 
 function renderScene(){
@@ -299,36 +292,6 @@ function renderScene(){
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-
-    // Original examples
-
-    // var body = new Cube();
-    // body.color = [1.0, 0.0, 0.0, 1.0];
-    // body.matrix.translate(-0.25, -0.75, 0.0);
-    // body.matrix.rotate(-5, 1, 0, 0);
-    // body.matrix.scale(0.5, 0.3, 0.5);
-    // body.render();
-
-    // var yellow = new Cube();
-    // yellow.color = [1.0, 1.0, 0.0, 1.0];
-    // yellow.matrix.setTranslate(0, -0.5, 0.0);
-    // yellow.matrix.rotate(-5, 1, 0, 0);
-    // yellow.matrix.rotate(g_yellowAngle, 0, 0, 1);
-    // var yellowCoordinatesMatrix = new Matrix4(yellow.matrix);
-    // yellow.matrix.scale(0.25, 0.7, 0.5);
-    // yellow.matrix.translate(-0.5, 0, 0.0);
-    // yellow.render();
-
-
-    // var magenta = new Pyramid();
-    // magenta.color = [1.0, 0.0, 1.0, 1.0];
-    // // magenta.matrix = yellowCoordinatesMatrix;
-    // // magenta.matrix.translate(0, 0.65, 0.0, 0);
-    // // magenta.matrix.rotate(g_MagentaAngle, 0, 0, 1);
-    // magenta.matrix.scale(0.3, 0.3, 0.3);
-    // // magenta.matrix.translate(-0.5, 0, -0.001, 0);
-    // magenta.render();
-
     // Dog's Head
     var dogHead = new Cube();
     dogHead.color = [0.8, 0.7, 0.5, 1.0];
@@ -337,7 +300,6 @@ function renderScene(){
     dogHead.matrix.rotate(g_BodyAngle * 0.125, 1, 0, 0);
     var dogHeadCoordsMatrix1 = new Matrix4(dogHead.matrix);
     var dogHeadCoordsMatrix2 = new Matrix4(dogHead.matrix);
-    // dogHead.matrix.rotate(180, 0, 1, 0)
     dogHead.matrix.scale(0.5, 0.5, 0.45);
     dogHead.render();
 
@@ -347,7 +309,6 @@ function renderScene(){
     dogMouth.color = [0.35, 0.2, 0.1, 1.0];
     dogMouth.matrix = dogHeadCoordsMatrix1;
     dogMouth.matrix.translate(0.05, 0, -0.15);
-    // dogMouth.matrix.rotate(0, 1, 0, 0);
     dogMouth.matrix.scale(0.4, 0.3, 0.15);
     dogMouth.render();
 
@@ -403,26 +364,8 @@ function renderScene(){
    rightEar.matrix = dogRightEarCoordsMatrix;
    rightEar.matrix.rotate(180, 0, 0, 1);
    rightEar.matrix.translate(-1.0, -1.025, -0.625);
-//    rightEar.matrix.scale(1.5, 1.5, 1.5); // Make it smaller
    rightEar.render();
 
-//    var dogRightEar = new Cube();
-//    dogRightEar.color = [0.4, 0.7, 0.4, 1.0];
-//    dogRightEar.matrix = dogHeadCoordsMatrix2
-//    dogRightEar.matrix.translate(0.15, 0.5, -0.6);
-//    dogRightEar.matrix.rotate(0, 1, 0, 0);
-//    dogRightEar.matrix.scale(0.1, 0.1, 0.15);
-//    dogRightEar.render();
-
-
-    // var rightEye = new Cube();
-    // rightEye.color = [0.1, 0.1, 0.1, 1.0];
-    // rightEye.matrix = dogHeadCoordsMatrix1;
-    // rightEye.matrix.translate(0.0625, 0.25, -0.8);
-    // rightEye.matrix.rotate(0, 1, 0, 0);
-    // rightEye.matrix.scale(0.1, 0.1, 0.1);
-    // rightEye.render();
-    
     // Dog's Body
     var torsoFront = new Cube();
     torsoFront.color = [0.8, 0.7, 0.5, 1.0];
